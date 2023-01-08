8ee0d0f221f1aaa25b982dfc2b4256e658d6f473
Arwa Mohammed <amalghamdi22@twq.idserve.net>
2023-01-08 10:11:25 +0300

    first commit

---
+++ Listit/Listit/ListitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListitApp.swift
+//  Listit
+//
+//  Created by Arwamohammed07 on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ListitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
